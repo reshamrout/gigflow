36501bbaae44f1fc9a508c6b17cfbdb0d34c9151
Resham Rout <reshamrout@gmail.com>
2026-01-14 15:41:20 +0530

    Deploment changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -53,6 +53,8 @@ const login = async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true, 
+      sameSite: none
     };
     user.password = ''
     res.cookie("token", token, options).status(200).json({
