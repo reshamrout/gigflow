524e2566b455c91b3c00ac7bb94d369462f325a6
Resham Rout <reshamrout@gmail.com>
2026-01-14 22:48:45 +0530

    fix: fixed login issue

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,7 +54,7 @@ const login = async (req, res) => {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: true, 
-      sameSite: none
+      sameSite: "none"
     };
     user.password = ''
     res.cookie("token", token, options).status(200).json({
